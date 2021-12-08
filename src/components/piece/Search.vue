e5f16a40cd30ce2f4665ee9996d4e472ae431c0a
jiyeon1117 <alice031117@naver.com>
2021-12-08 15:40:56 +0900

    Search Component piece

---
+++ src/components/piece/Search.vue
@@ -0,0 +1,32 @@
+<template>
+  <input type="text" name="search" id="search" placeholder="Search">
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+#search{
+  width: 192px;
+  height: 37px;
+  margin-right: 37px;
+  margin-bottom: 10px;
+  border: 2px solid #E1E7EE;
+  border-radius: 10px;
+  background-color: #F5F8FA;
+
+  font-family: 'Roboto', sans-serif;
+  font-weight: 500;
+  font-size: 17px;
+  outline: none;
+  padding-left: 8px;
+}
+
+input::placeholder {
+  color: #557EA7;
+}
+
+</style>
