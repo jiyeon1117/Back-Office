0b0b09905f58612e836a9414b23ae7a8b7fb7f02
jiyeon1117 <alice031117@naver.com>
2021-12-08 15:39:50 +0900

    Server Page CSS Complete

---
+++ src/components/Server.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="title Eng">Server</div>
+    <div class="Box">
+      <div class="box-title">서버 통신 상태</div>
+      <Search class="search"></Search>
+      <table>
+        <tr>
+          <th>서버</th>
+          <th>IP</th>
+          <th>동작여부</th>
+        </tr>
+        <tr v-for="i in 10" :key="i">
+          <td>서버</td>
+          <td>IP</td>
+          <td>동작여부</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import Search from './piece/Search.vue'
+
+export default {
+  components: {
+    'Search' : Search
+  }
+}
+</script>
+
+<style scoped>
+table {
+  margin: 15px 0px 0px 37px;
+}
+.search{
+  margin-top: -10px;
+  float: right;
+}
+
+</style>
