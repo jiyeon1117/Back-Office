b10b545202e6cf7580ae263bfdb22680aed7150b
jiyeon1117 <alice031117@naver.com>
2021-12-08 15:40:23 +0900

    Search Commponent piece

---
+++ src/components/piece/Select.vue
@@ -0,0 +1,28 @@
+<template>
+  <select name="select" id="select">
+    <option value="value">value</option>
+  </select>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+#select{
+  width: 100px;
+  height: 37px;
+  border: 2px solid #003366;
+  border-radius: 10px;
+  padding-left: 8px;
+  font-family: 'Noto Sans KR', sans-serif;
+  font-weight: 500;
+  font-size: 16px;
+  color: #003366;
+  outline: none;
+  vertical-align: middle;
+}
+
+</style>
