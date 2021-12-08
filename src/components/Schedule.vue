236a7bf4f52c29b0f0df9197e3e475ff12f925f8
jiyeon1117 <alice031117@naver.com>
2021-12-08 15:41:34 +0900

    Send & Schedule Page

---
+++ src/components/Schedule.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="title Kor">스케쥴 설정</div>
+    <div class="Box">
+      <div class="box-title">스케쥴 설정</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Send.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="title Kor">재전송 명령</div>
+    <div class="Box">
+      <div class="box-title">재전송 명령</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
